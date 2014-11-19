89c30c05ed88d8ec8fa73104b55c7bf05156ce50
wangkemiao <wangkemiao@baidu.com>
2014-11-19 14:29:05 +0800

    转为edp project

---
+++ src/main.js
@@ -0,0 +1,15 @@
+/**
+ * @file FC 基础层
+ * @author Leo Wang(wangkemiao@baidu.com)
+ */
+
+define(function (require) {
+
+    /**
+     * FC 基础层
+     * @module fc
+     */
+    var fc = {};
+
+    return fc;
+});
